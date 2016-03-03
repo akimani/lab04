50c889729c050847e86f851d422ef9b2b8310583
Alexander Kimani <a906k620@ku.edu>
2016-03-03 09:50:15 -0600

    Fix format and wording

--- Quiz.php
+++ Quiz.php
@@ -1,5 +1,8 @@
 <?php
 //access the global array called $_POST to get the values from the text fields
 $answer1  = $_POST["a"];
+echo "Question1: What is the capital of Kansas:?<br>";
 echo "You answered: ".$answer1. "<br>";
+echo "Correct answer: Topeka <br> <br>";
+
 ?>
