24d1edc57621a1aea2ae2d04401fc0ed436aa878
Alexander Kimani <a906k620@ku.edu>
2016-03-03 10:01:53 -0600

    Add score tracker >php

--- Quiz.php
+++ Quiz.php
@@ -1,8 +1,14 @@
 <?php
 //access the global array called $_POST to get the values from the text fields
-$answer1  = $_POST["a"];
+$total = 0;
+$answer1 = $_POST["a"];
+
 echo "Question1: What is the capital of Kansas:?<br>";
 echo "You answered: ".$answer1. "<br>";
-echo "Correct answer: Topeka <br> <br>";
+echo "Correct answer: Topeka <br> <br><br>";
+if($answer1 == "Topeka")
+    $total++;
+echo "Total " .$total. "<br>";
+
 
 ?>
