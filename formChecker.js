cd3a1fe19ec2e29754cef960ab0935001c85bf9a
Alexander Kimani <a906k620@ku.edu>
2016-03-03 20:32:08 -0600

    Validate the quantities

--- formChecker.js
+++ formChecker.js
@@ -2,6 +2,9 @@ function Checkout()
 {
     var user = document.getElementById("user").value;
 	var passwd = document.getElementById("password").value;
+	var qdsktop = document.getElementById("dsktop").value;
+    var qlptop = document.getElementById("lptop").value;
+    var qcellp = document.getElementById("cphone").value;
 
 	var email = /\b\w+@\w+\.com\b/;
 
@@ -15,6 +18,12 @@ function Checkout()
     if(passwd =="")
     {
         window.alert("Password cannot be blank");
+        return false;
+    }
+
+    if(qdsktop<0 || isNaN(qdsktop) || qlptop<0 || isNaN(qlptop) || qcellp<0 || isNaN(qcellp))
+    {
+        window.alert("Invalid quantity(s)");
         return false;
     }
 }
